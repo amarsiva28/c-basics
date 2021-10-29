a69a426e1204c4b7c3aea82a0e1b1f6c7fa09d0f
amarsiva28 <92994268+amarsiva28@users.noreply.github.com>
2021-10-29 00:00:23 -0700

    Add files via upload

---
+++ c language amar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//varible declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	// Initialization
+	age = 17;
+	cgpa = 8.0;
+	initial = 'M';
+	
+	//ouptut
+	printf("age of the student %d\n", age);
+	printf("cgpa of the student %f\n", cgpa);
+	printf("student surname start with %c", initial);
+}
