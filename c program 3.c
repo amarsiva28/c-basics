a0aaaa7f7901d678021e9a6c7d97ae14ec89bcc3
amarsiva28 <92994268+amarsiva28@users.noreply.github.com>
2021-10-29 23:48:26 -0700

    Add files via upload

--- c program 3.c
+++ c program 3.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 void main()
 {
-	
-	float num1 = 10, num2 = 20, sum = num1+num2;
-	
-	// output
-	printf("sum of %f and %f is %f",num1,num2, sum);
+	float num1 = 2.5, num2 = 1.5, sum = num1+num2;
+	//output
+	printf(" sum of %f and %f is %f",num1,num2, sum);
 }
